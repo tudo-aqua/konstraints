1c7d79c09da918de9bb1efe1c81b2ec7074830a3
feho243 <felix.holewa@tu-dortmund.de>
2022-12-06 09:33:59 +0100

    Adds KonstraintsRunner

--- src/main/kotlin/tools/aqua/constraints/runner/KonstraintsRunner.kt
+++ src/main/kotlin/tools/aqua/constraints/runner/KonstraintsRunner.kt
@@ -20,73 +20,51 @@ package tools.aqua.constraints.runner
  * limitations under the License.
  */
 
-import gov.nasa.jpf.constraints.api.ConstraintSolver
-import gov.nasa.jpf.constraints.api.SolverContext
-import gov.nasa.jpf.constraints.api.Valuation
-import gov.nasa.jpf.constraints.exceptions.UndecidedBooleanExeception
-import gov.nasa.jpf.constraints.smtlibUtility.SMTProblem
-import gov.nasa.jpf.constraints.solvers.ConstraintSolverFactory
-import jdk.internal.org.jline.reader.impl.DefaultParser
+import com.github.h0tk3y.betterParse.grammar.parseToEnd
 import org.apache.commons.cli.CommandLine
-import org.apache.commons.cli.CommandLineParser
+import org.apache.commons.cli.DefaultParser
 import org.apache.commons.cli.Option
 import org.apache.commons.cli.Options
-import org.apache.commons.cli.ParseException
-import structuralEquivalence.Processor
+import tools.aqua.constraints.smtlib.SMTLibParser
+import tools.aqua.constraints.z3.Z3SMTLibSolver
 import java.io.File
 
-
-class KonstraintsRunner {
-    private fun runProgram(cmd: CommandLine) {
-        val filepath: String = cmd.getOptionValue("smt")
-        val solver: String = cmd.getOptionValue("s")
-        if (solver.equals("z3", ignoreCase = true)
-            || solver.equals("cvc5", ignoreCase = true)
-            || solver.equals("multi", ignoreCase = true)
-        ) {
-            val constraintSolver: ConstraintSolver = ConstraintSolverFactory.createSolver(solver)
-            val problem: SMTProblem = Processor.parseFile(File(filepath))
-            val ctx: SolverContext = constraintSolver.createContext()
-            val valuation = Valuation()
-            ctx.add(problem.assertions)
-            val res: ConstraintSolver.Result = ctx.solve(valuation`)
-            println("RESULT: $res")
-            if (res === ConstraintSolver.Result.SAT) {
-                var evaluated = false
-                try {
-                    System.out.println("Valuation: " + valuation.toString())
-                    evaluated = problem.getAllAssertionsAsConjunction().evaluateSMT(valuation)
-                } catch (e: UndecidedBooleanExeception) {
-                    evaluated = true
-                } catch (t: Throwable) {
-                    t.printStackTrace()
-                    println("VALUATIONERROR")
-                }
-                println("EVALUATED: $evaluated")
-            }
-        }
-        System.exit(0)
-    }
-
-    companion object {
-        @JvmStatic
-        fun main(args: Array<String>) {
-            val parser: CommandLineParser = DefaultParser()
-            try {
-                val cmd: CommandLine = parser.parse(setupOptions(), args)
-                KonstraintsRunner().runProgram(cmd)
-            } catch (e: ParseException) {
-                e.printStackTrace()
-            }
-        }
-
-        private fun setupOptions(): Options {
-            val smtRootFolder: Option = Option.builder("smt").longOpt("smt_file").hasArg().required().build()
-            val solver: Option = Option.builder("s").longOpt("solver").hasArg().required().build()
-            val checkerOptions = Options()
-            checkerOptions.addOption(smtRootFolder)
-            checkerOptions.addOption(solver)
-            return checkerOptions
-        }
-    }
+fun main(args: Array<String>) {
+    val parser = DefaultParser()
+    val cmd: CommandLine = parser.parse(setupOptions(), args)
+    runProgram(cmd)
 }
+
+fun runProgram(cmd: CommandLine) {
+    val parser = SMTLibParser()
+    val filepath: String = cmd.getOptionValue("smt")
+    val solver: String = cmd.getOptionValue("s")
+    val smtProblem = File(filepath).readText()
+
+    if (solver.equals("z3", ignoreCase = true)) {
+        val constraintSolver = Z3SMTLibSolver()
+        println(smtProblem)
+        val problem = parser.parseToEnd(smtProblem)
+        val solverResult = problem.execute(constraintSolver).toString()
+
+        println("RESULT: $solverResult")
+    } else if (solver.equals("multi", ignoreCase = true)) {
+        println("Not yet supported for multi.")
+    } else if (solver.equals("cvc5", ignoreCase = true)) {
+        println("Not yet supported for cvc5.")
+    }
+    System.exit(0)
+}
+
+
+fun setupOptions(): Options {
+    val smtRootFolder: Option = Option.builder("smt").longOpt("smt_file").hasArg().required().build()
+    val solver: Option = Option.builder("s").longOpt("solver").hasArg().required().build()
+    val checkerOptions = Options()
+    checkerOptions.addOption(smtRootFolder)
+    checkerOptions.addOption(solver)
+    return checkerOptions
+}
+
+
+
