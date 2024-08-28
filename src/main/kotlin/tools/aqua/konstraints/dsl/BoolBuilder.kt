3ffbcb01eb2209111194cedf2c661598db469873
laurenzlevi <laurenzlevi@users.noreply.github.com>
2024-08-28 12:05:36 +0200

    Update BoolBuilder.kt

--- src/main/kotlin/tools/aqua/konstraints/dsl/BoolBuilder.kt
+++ src/main/kotlin/tools/aqua/konstraints/dsl/BoolBuilder.kt
@@ -68,44 +68,18 @@ infix fun<T : Sort> Expression<T>.distinct(other: Expression<T>): Distinct =
         Distinct(this, other)
     }
 
-fun Builder<BoolSort>.not(block: Builder<BoolSort>.() -> Expression<BoolSort>): Not {
-    val expr = Builder<BoolSort>().block()
-
-    val op = Not(expr)
-    this.children.add(op)
-
-    return op
-}
-
-fun Builder<BoolSort>.and(init: Builder<BoolSort>.() -> Unit): And {
+private fun Builder<BoolSort>.makeBoolOperator(init: Builder<BoolSort>.() -> Unit, op: (List<Expression<BoolSort>>) -> Expression<BoolSort>): Expression<BoolSort> {
     val builder = Builder<BoolSort>()
     builder.init()
 
-    val op = And(builder.children)
-    this.children.add(op)
+    this.children.add(op(builder.children))
 
-    return op
+    return this.children.last()
 }
 
-fun Builder<BoolSort>.or(init: Builder<BoolSort>.() -> Unit): Or {
-    val builder = Builder<BoolSort>()
-    builder.init()
-
-    val op = Or(builder.children)
-    this.children.add(op)
-
-    return op
-}
-
-fun Builder<BoolSort>.xor(init: Builder<BoolSort>.() -> Unit): XOr {
-    val builder = Builder<BoolSort>()
-    builder.init()
-
-    val op = XOr(builder.children)
-    this.children.add(op)
-
-    return op
-}
+fun Builder<BoolSort>.and(init: Builder<BoolSort>.() -> Unit) = this.makeBoolOperator(init, ::And)
+fun Builder<BoolSort>.or(init: Builder<BoolSort>.() -> Unit) = this.makeBoolOperator(init, ::Or)
+fun Builder<BoolSort>.xor(init: Builder<BoolSort>.() -> Unit) = this.makeBoolOperator(init, ::XOr)
 
 fun<T : Sort> Builder<BoolSort>.eq(init: Builder<T>.() -> Unit): Equals {
     val builder = Builder<T>()
@@ -125,4 +99,16 @@ fun<T : Sort> Builder<BoolSort>.distinct(init: Builder<T>.() -> Unit): Equals {
     this.children.add(op)
 
     return op
+}
+
+fun Builder<BoolSort>.not(block: Builder<BoolSort>.() -> Expression<BoolSort>): Not {
+    this.children.add(Not(Builder<BoolSort>().block()))
+
+    return this.children.last() as Not
+}
+
+fun Builder<BoolSort>.isInt(block: Builder<RealSort>.() -> Expression<RealSort>): IsInt {
+    this.children.add(IsInt(Builder<RealSort>().block()))
+
+    return this.children.last() as IsInt
 }
