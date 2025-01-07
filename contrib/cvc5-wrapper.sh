39da3c360c8f2793a314fe0c5dfcc493d9d480da
laurenzlevi <72398071+laurenzlevi@users.noreply.github.com>
2025-01-07 22:48:04 +0100

    Updated copyright notices

--- contrib/cvc5-wrapper.sh
+++ contrib/cvc5-wrapper.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #  SPDX-License-Identifier: Apache-2.0
 #
-#  Copyright 2023-2024 The Konstraints Authors
+#  Copyright 2023-2025 The Konstraints Authors
 #
 #  Licensed under the Apache License, Version 2.0 (the "License");
 #  you may not use this file except in compliance with the License.
