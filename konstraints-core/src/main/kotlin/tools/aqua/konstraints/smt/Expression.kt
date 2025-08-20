e000c7596cdc16adf62a8d84d768fd8b53898906
laurenzlevi <72398071+laurenzlevi@users.noreply.github.com>
2025-08-20 19:44:29 +0200

    Update Expression.kt

--- konstraints-core/src/main/kotlin/tools/aqua/konstraints/smt/Expression.kt
+++ konstraints-core/src/main/kotlin/tools/aqua/konstraints/smt/Expression.kt
@@ -30,29 +30,28 @@ sealed interface Expression<out T : Sort> {
   // TODO implement more operations like filter, filterIsInstance, filterIsSort, forEach, onEach
   // etc.
 
-    /**
-     * Transform [this] and all [this.children] by applying [transformation].
-     *
-     * @return transformed expression
-     */
-    /*
-     * lambda can not be more type safe as it needs to apply to all expressions in the expression tree
-     */
-    fun transform(
-    transformation: (Expression<*>) -> Expression<*>
-    ): Expression<T> {
-        // transform all children
-        val transformedChildren = this.children.map { it.transform(transformation) }
+  /**
+   * Transform [this] and all [this.children] by applying [transformation].
+   *
+   * @return transformed expression
+   */
+  /*
+   * lambda can not be more type safe as it needs to apply to all expressions in the expression tree
+   */
+  fun transform(transformation: (Expression<*>) -> Expression<*>): Expression<T> {
+    // transform all children
+    val transformedChildren = this.children.map { it.transform(transformation) }
 
-        // check if any child was copied
-        return (if ((transformedChildren zip this.children).any { (new, old) -> new !== old }) {
-            // return copied expression with new children
-            transformation(this.copy(transformedChildren))
-        } else {
-            // transform this expression, prevent it from changing the sort
-            transformation(this)
-        }) as Expression<T>
-    }
+    // check if any child was copied
+    return (if ((transformedChildren zip this.children).any { (new, old) -> new !== old }) {
+      // return copied expression with new children
+      transformation(this.copy(transformedChildren))
+    } else {
+      // transform this expression, prevent it from changing the sort
+      transformation(this)
+    })
+        as Expression<T>
+  }
 
   /**
    * Evaluate [predicate] on this expression and all sub-expressions.
